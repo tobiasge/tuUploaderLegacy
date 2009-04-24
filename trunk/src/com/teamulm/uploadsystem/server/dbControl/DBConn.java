047480d3fb849aadf4509d70606f5e28f63559a7
Tobias Genannt <tobias.genannt@gmail.com>
2009-04-24 06:12:18 +0000

    - code formatting
    git-svn-id: svn://helios.nb.team-ulm.de/uploadSystem@123 a202d7e1-1033-44dc-8c76-cd45ceb0298f

--- trunk/src/com/teamulm/uploadsystem/server/dbControl/DBConn.java
+++ trunk/src/com/teamulm/uploadsystem/server/dbControl/DBConn.java
@@ -13,134 +13,125 @@ import org.apache.log4j.Logger;
 
 public class DBConn {
 
-    private static final Logger log = Logger.getLogger(com.teamulm.uploadsystem.server.dbControl.DBConn.class);
+	private static final Logger log = Logger.getLogger(com.teamulm.uploadsystem.server.dbControl.DBConn.class);
 
-    /** ******************************************* */
-    /** ***************ServerConfig**************** */
-    private String dbDriv;
+	/** ******************************************* */
+	/** ***************ServerConfig**************** */
+	private String dbDriv;
 
-    private String dbUser;
+	private String dbUser;
 
-    private String dbPass;
+	private String dbPass;
 
-    private String dbURL;
-    private String serverName;
-    /** ******************************************* */
+	private String dbURL;
+	private String serverName;
+	/** ******************************************* */
 
-    private Connection mySQL;
+	private Connection mySQL;
 
-    private Timer keepAliveTimer;
+	private Timer keepAliveTimer;
 
-    protected DBConn(String dbURL, String dbUser, String dbPass, String dbDriv,
-	    String serverName) {
-	this.serverName = serverName;
-	this.dbDriv = dbDriv;
-	this.dbPass = dbPass;
-	this.dbURL = dbURL;
-	this.dbUser = dbUser;
-	this.connect();
-	keepAliveTimer = new Timer("KeepAlive Timer for " + this.serverName,
-		true);
-	keepAliveTimer.scheduleAtFixedRate(new KeepAliveTimerTask(), 0,
-		1000 * 55);
-    }
-
-    private boolean connect() {
-	try {
-	    if (null != this.mySQL) {
-		this.mySQL.close();
-	    }
-	    Class.forName(this.dbDriv);
-	    this.mySQL = DriverManager.getConnection(this.dbURL, this.dbUser,
-		    this.dbPass);
-	    this.mySQL.setAutoCommit(true);
-	    return true;
-	} catch (Exception e) {
-	    log.error("Failure in connect: " + e.getClass());
-	    log.error("Failure in connect: " + e.getMessage());
-	    return false;
-	}
-    }
-
-    protected Connection getConnection() {
-	return this.mySQL;
-    }
-
-    public ResultSet executeQuery(String query) {
-	try {
-	    Statement stmt = this.mySQL.createStatement();
-	    return stmt.executeQuery(query);
-	} catch (SQLException e) {
-	    log.error("Could not execute query for " + this.serverName);
-	    return null;
-	}
-    }
-
-    public int executeUpdate(String query) {
-	try {
-	    Statement stmt = this.mySQL.createStatement();
-	    return stmt.executeUpdate(query);
-	} catch (SQLException e) {
-	    log.error("Could not execute update for " + this.serverName);
-	    return -1;
-	}
-    }
-
-    public PreparedStatement prepareStatement(String query) {
-	try {
-	    return this.getConnection().prepareStatement(query);
-	} catch (SQLException SQLEx) {
-	    log.error("Could not prepare statement for " + this.serverName);
-	    return null;
-	}
-    }
-
-    private boolean isConnected() {
-	try {
-	    if (null == this.mySQL || this.mySQL.isClosed()
-		    || this.mySQL.isReadOnly()) {
-		log.warn("Connection to " + this.serverName + " lost");
-		return false;
-	    } else {
-		this.mySQL.createStatement().executeQuery("SELECT NOW()");
-		return true;
-	    }
-	} catch (Exception e) {
-	    log.warn("Connection to " + this.serverName + " lost");
-	    return false;
-	}
-    }
-
-    class KeepAliveTimerTask extends TimerTask {
-
-	int counter;
-
-	public KeepAliveTimerTask() {
-	    this.counter = 0;
+	protected DBConn(String dbURL, String dbUser, String dbPass, String dbDriv, String serverName) {
+		this.serverName = serverName;
+		this.dbDriv = dbDriv;
+		this.dbPass = dbPass;
+		this.dbURL = dbURL;
+		this.dbUser = dbUser;
+		this.connect();
+		keepAliveTimer = new Timer("KeepAlive Timer for " + this.serverName, true);
+		keepAliveTimer.scheduleAtFixedRate(new KeepAliveTimerTask(), 0, 1000 * 55);
 	}
 
-	@Override
-	public void run() {
-	    try {
-		if (!DBConn.this.isConnected()) {
-		    DBConn.this.connect();
+	private boolean connect() {
+		try {
+			if (null != this.mySQL) {
+				this.mySQL.close();
+			}
+			Class.forName(this.dbDriv);
+			this.mySQL = DriverManager.getConnection(this.dbURL, this.dbUser, this.dbPass);
+			this.mySQL.setAutoCommit(true);
+			return true;
+		} catch (Exception e) {
+			log.error("Failure in connect: " + e.getClass());
+			log.error("Failure in connect: " + e.getMessage());
+			return false;
 		}
-		DBConn.this.mySQL.createStatement()
-			.executeQuery("SELECT NOW()");
-		this.counter++;
-		if (0 == (this.counter % 100)) {
-		    this.counter = 0;
-		    log.info("Success in KeepAliveTimerTask for "
-			    + DBConn.this.serverName);
-		}
-	    } catch (Exception e) {
-		log.warn("Failure in KeepAliveTimerTask for "
-			+ DBConn.this.serverName + ": " + e.getClass());
-		log.warn("Failure in KeepAliveTimerTask for "
-			+ DBConn.this.serverName + ": " + e.getMessage());
-
-		DBConn.this.connect();
-	    }
 	}
-    }
+
+	protected Connection getConnection() {
+		return this.mySQL;
+	}
+
+	public ResultSet executeQuery(String query) {
+		try {
+			Statement stmt = this.mySQL.createStatement();
+			return stmt.executeQuery(query);
+		} catch (SQLException e) {
+			log.error("Could not execute query for " + this.serverName);
+			return null;
+		}
+	}
+
+	public int executeUpdate(String query) {
+		try {
+			Statement stmt = this.mySQL.createStatement();
+			return stmt.executeUpdate(query);
+		} catch (SQLException e) {
+			log.error("Could not execute update for " + this.serverName);
+			return -1;
+		}
+	}
+
+	public PreparedStatement prepareStatement(String query) {
+		try {
+			return this.getConnection().prepareStatement(query);
+		} catch (SQLException SQLEx) {
+			log.error("Could not prepare statement for " + this.serverName);
+			return null;
+		}
+	}
+
+	private boolean isConnected() {
+		try {
+			if (null == this.mySQL || this.mySQL.isClosed() || this.mySQL.isReadOnly()) {
+				log.warn("Connection to " + this.serverName + " lost");
+				return false;
+			} else {
+				this.mySQL.createStatement().executeQuery("SELECT NOW()");
+				return true;
+			}
+		} catch (Exception e) {
+			log.warn("Connection to " + this.serverName + " lost");
+			return false;
+		}
+	}
+
+	class KeepAliveTimerTask extends TimerTask {
+
+		int counter;
+
+		public KeepAliveTimerTask() {
+			this.counter = 0;
+		}
+
+		@Override
+		public void run() {
+			try {
+				if (!DBConn.this.isConnected()) {
+					DBConn.this.connect();
+				}
+				DBConn.this.mySQL.createStatement().executeQuery("SELECT NOW()");
+				this.counter++;
+				if (0 == (this.counter % 100)) {
+					this.counter = 0;
+					log.info("Success in KeepAliveTimerTask for " + DBConn.this.serverName);
+				}
+			} catch (Exception e) {
+				log.warn("Failure in KeepAliveTimerTask for " + DBConn.this.serverName + ": " + e.getClass());
+				log.warn("Failure in KeepAliveTimerTask for " + DBConn.this.serverName + ": " + e.getMessage());
+
+				DBConn.this.connect();
+			}
+		}
+	}
 }
