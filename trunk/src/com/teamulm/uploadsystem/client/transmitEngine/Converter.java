d6f3d1fe90c87e63959caaad760c56b32a848230
Tobias Genannt <tobias.genannt@gmail.com>
2009-06-06 08:33:35 +0000

    - added non-nls markers - using messages
    git-svn-id: svn://helios.nb.team-ulm.de/uploadSystem@189 a202d7e1-1033-44dc-8c76-cd45ceb0298f

--- trunk/src/com/teamulm/uploadsystem/client/transmitEngine/Converter.java
+++ trunk/src/com/teamulm/uploadsystem/client/transmitEngine/Converter.java
@@ -18,6 +18,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.text.MessageFormat;
 
 import javax.imageio.ImageIO;
 
@@ -26,6 +27,7 @@ import org.apache.log4j.Logger;
 import com.teamulm.uploadsystem.client.Helper;
 import com.teamulm.uploadsystem.client.TeamUlmUpload;
 import com.teamulm.uploadsystem.client.gui.MainWindow;
+import com.teamulm.uploadsystem.client.gui.Messages;
 import com.teamulm.uploadsystem.client.imageProcessing.ImageConverter;
 import com.teamulm.uploadsystem.client.imageProcessing.ImageConverterFactory;
 
@@ -41,11 +43,11 @@ public class Converter extends Thread {
 
 	private int ident;
 
-	private final String fileSep = System.getProperty("file.separator");
+	private final String fileSep = System.getProperty("file.separator"); //$NON-NLS-1$
 
-	private String bigPicName = "pic";
+	private String bigPicName = "pic"; //$NON-NLS-1$
 
-	private String smallPicName = "s_pic";
+	private String smallPicName = "s_pic"; //$NON-NLS-1$
 
 	private ImageConverter myImageConverter;
 
@@ -55,8 +57,8 @@ public class Converter extends Thread {
 
 	public Converter(TrmEngine chef, int ident) {
 		super();
-		this.setName("Converter " + ident);
-		this.savePath = System.getProperty("java.io.tmpdir");
+		this.setName("Converter " + ident); //$NON-NLS-1$
+		this.savePath = System.getProperty("java.io.tmpdir"); //$NON-NLS-1$
 		this.chef = chef;
 		this.stopRequest = false;
 		this.ident = ident;
@@ -82,7 +84,8 @@ public class Converter extends Thread {
 
 	@Override
 	public void run() {
-		TeamUlmUpload.getInstance().getMainWindow().addStatusLine("Beginne Konvertierung: " + this.ident);
+		TeamUlmUpload.getInstance().getMainWindow().addStatusLine(
+			MessageFormat.format(Messages.getString("Converter.logMessages.startingConvert"), this.ident)); //$NON-NLS-1$
 		File outBigPicName = null;
 		File outSmaPicName = null;
 		File actFile = null;
@@ -91,14 +94,15 @@ public class Converter extends Thread {
 			while (!this.stopRequest && ((actFile = this.chef.getNextToConvert()) != null)) {
 				BufferedImage actPic = ImageIO.read(actFile);
 				if ((actPic.getWidth() < this.bigPicSize.width) && (actPic.getHeight() < this.bigPicSize.height)) {
-					log.info("Thread: " + this.ident + " Übersprungen wegen Größe " + actFile.getName() + " -> Breit: "
-						+ actPic.getWidth() + " Hoch: " + actPic.getHeight());
-					TeamUlmUpload.getInstance().getMainWindow().addStatusLine("Bild ignoriert: Bild zu klein.");
+					log.info("Thread: " + this.ident + " Übersprungen wegen Größe " + actFile.getName() + " -> Breit: " //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+						+ actPic.getWidth() + " Hoch: " + actPic.getHeight()); //$NON-NLS-1$
+					TeamUlmUpload.getInstance().getMainWindow().addStatusLine(
+						Messages.getString("Converter.logMessages.picTooSmall")); //$NON-NLS-1$
 				} else if ((actPic.getWidth() == this.bigPicSize.width)
 					&& (actPic.getHeight() == this.bigPicSize.height)) {
 					number = this.chef.getNextPicNum();
-					outBigPicName = new File(this.savePath + this.fileSep + this.bigPicName + number + ".jpg");
-					outSmaPicName = new File(this.savePath + this.fileSep + this.smallPicName + number + ".jpg");
+					outBigPicName = new File(this.savePath + this.fileSep + this.bigPicName + number + ".jpg"); //$NON-NLS-1$
+					outSmaPicName = new File(this.savePath + this.fileSep + this.smallPicName + number + ".jpg"); //$NON-NLS-1$
 					if (this.copyFile(actFile, outBigPicName)
 						&& this.myImageConverter.createPreview(outBigPicName, outSmaPicName)) {
 
@@ -109,13 +113,13 @@ public class Converter extends Thread {
 				} else {
 					if (!this.myImageConverter.isKownFormat(actPic.getWidth(), actPic.getHeight())) {
 						TeamUlmUpload.getInstance().getMainWindow().addStatusLine(
-							"Bild ignoriert: Bild hat falsches Format.");
+							Messages.getString("Converter.logMessages.wrongFormat")); //$NON-NLS-1$
 						this.chef.fileWasIgnored();
 						continue;
 					}
 					number = this.chef.getNextPicNum();
-					outBigPicName = new File(this.savePath + this.fileSep + this.bigPicName + number + ".jpg");
-					outSmaPicName = new File(this.savePath + this.fileSep + this.smallPicName + number + ".jpg");
+					outBigPicName = new File(this.savePath + this.fileSep + this.bigPicName + number + ".jpg"); //$NON-NLS-1$
+					outSmaPicName = new File(this.savePath + this.fileSep + this.smallPicName + number + ".jpg"); //$NON-NLS-1$
 					if (this.myImageConverter.createPic(actFile, outBigPicName)
 						&& this.myImageConverter.createPreview(outBigPicName, outSmaPicName)) {
 						this.chef.setToTransmit(outSmaPicName);
@@ -126,8 +130,10 @@ public class Converter extends Thread {
 			}
 			sleep(5);
 		} catch (Exception e) {
+			log.error("", e); //$NON-NLS-1$
 			TeamUlmUpload.getInstance().getMainWindow().addStatusLine(
-				"Thread: " + this.ident + " Konnte " + actFile.getName() + " nicht konvertieren");
+				MessageFormat.format(Messages.getString("Converter.logMessages.notConverted"), actFile.getName(), //$NON-NLS-1$
+					this.ident));
 			Helper.getInstance().systemCrashHandler(e);
 		}
 		TeamUlmUpload.getInstance().getMainWindow().setConvertProgress(MainWindow.PROGRESS_BAR_MAX);
