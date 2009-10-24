20e3221bd5adf6d1441feb159fb547a7d0384119
Tobias Genannt <tobias.genannt@gmail.com>
2009-10-24 22:10:10 +0000

    - implemented hashCode() and equals()
    git-svn-id: svn://helios.nb.team-ulm.de/uploadSystem@205 a202d7e1-1033-44dc-8c76-cd45ceb0298f

--- trunk/src/com/teamulm/uploadsystem/data/Location.java
+++ trunk/src/com/teamulm/uploadsystem/data/Location.java
@@ -3,6 +3,8 @@ package com.teamulm.uploadsystem.data;
 import java.io.Serializable;
 
 import org.apache.commons.lang.builder.CompareToBuilder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
@@ -20,6 +22,22 @@ public class Location implements Serializable, Comparable<Location> {
 		this.name = name;
 	}
 
+	public int compareTo(Location rhs) {
+		Location lhs = this;
+		return new CompareToBuilder().append(lhs.name, rhs.name).toComparison();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (obj instanceof Location) {
+			Location rhs = (Location) obj;
+			Location lhs = (Location) this;
+			return new EqualsBuilder().append(lhs.id, rhs.id).append(lhs.name, rhs.name).isEquals();
+		}
+		return false;
+	}
+
 	public int getId() {
 		return id;
 	}
@@ -29,13 +47,13 @@ public class Location implements Serializable, Comparable<Location> {
 	}
 
 	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(15, 17).append(this.id).append(this.name).toHashCode();
+	}
+
+	@Override
 	public String toString() {
 		return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).append("Id", this.id).append("Name", //$NON-NLS-1$ //$NON-NLS-2$
 			this.name).toString();
-	}
-
-	public int compareTo(Location rhs) {
-		Location lhs = this;
-		return new CompareToBuilder().append(lhs.name, rhs.name).toComparison();
 	}
 }
