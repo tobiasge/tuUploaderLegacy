eab4f0e5b3599231bb8e2d9179f67db95ca01e7f
Tobias Genannt <tobias.genannt@gmail.com>
2010-06-25 14:20:50 +0000

    - test case - test pictures
    git-svn-id: svn://helios.nb.team-ulm.de/uploadSystem@264 a202d7e1-1033-44dc-8c76-cd45ceb0298f

--- trunk/test/com/teamulm/uploadsystem/client/imageProcessing/ImageConverterTest.java
+++ trunk/test/com/teamulm/uploadsystem/client/imageProcessing/ImageConverterTest.java
@@ -4,7 +4,10 @@ import java.awt.Dimension;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -63,15 +66,42 @@ public class ImageConverterTest {
 	@Test
 	public void createHqPictures() {
 		ImageConverter converter = new NoBlackBorderConverter(smallPicSize, bigPicSize, minHqPicSize, maxHqPicSize);
+		int picCount = 0;
+		for (File pic : this.getTestPictures()) {
+			File out = new File("./testOut/hq_" + picCount + ".jpg");
+			pic = converter.correctPictureOrientation(pic);
+			converter.createHqPic(pic, out);
+			picCount++;
+		}
 	}
 
 	@Test
 	public void createNormalPictures() {
 		ImageConverter converter = new NoBlackBorderConverter(smallPicSize, bigPicSize, minHqPicSize, maxHqPicSize);
+		int picCount = 0;
+		for (File pic : this.getTestPictures()) {
+			File out = new File("./testOut/bi_" + picCount + ".jpg");
+			File prev = new File("./testOut/sm_" + picCount + ".jpg");
+			pic = converter.correctPictureOrientation(pic);
+			converter.createPic(pic, out);
+			converter.createPreview(out, prev);
+			picCount++;
+		}
 	}
 
-	@Test
-	public void createSmallPictures() {
-		ImageConverter converter = new NoBlackBorderConverter(smallPicSize, bigPicSize, minHqPicSize, maxHqPicSize);
+	private List<File> getTestPictures() {
+		File testOut = new File("./testIn");
+		List<File> pictures = new ArrayList<File>();
+		for (File pic : testOut.listFiles(new FilenameFilter() {
+
+			@Override
+			public boolean accept(File basePath, String name) {
+				name = name.toLowerCase();
+				return name.endsWith(".jpg") || name.endsWith(".jpeg");
+			}
+		})) {
+			pictures.add(pic);
+		}
+		return pictures;
 	}
 }
